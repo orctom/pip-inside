99399aeb35cf710f608f958c1137085084547629
orctom <orctom@gmail.com>
2023-02-14 00:29:34 +0800

    fixed remove unused

--- pip_inside/commands/remove.py
+++ pip_inside/commands/remove.py
@@ -4,7 +4,7 @@ import sys
 
 import click
 
-from pip_inside.utils.dependencies import Package
+from pip_inside.utils.dependencies import Dependencies
 from pip_inside.utils.pyproject import PyProject
 
 
@@ -13,11 +13,8 @@ def handle_remove(name, group):
         pyproject = PyProject.from_toml()
         if pyproject.remove_dependency(name, group):
             pyproject.flush()
-            deps = Package.get_unused_sub_dependencies(name)
-            if deps:
-                cmd = [shutil.which('python'), '-m', 'pip', 'uninstall', name, *deps, '-y']
-            else:
-                cmd = [shutil.which('python'), '-m', 'pip', 'uninstall', name, '-y']
+            deps = Dependencies().get_unused_dependencies_for(name)
+            cmd = [shutil.which('python'), '-m', 'pip', 'uninstall', name, *deps, '-y']
             subprocess.run(cmd, stderr=sys.stderr, stdout=sys.stdout)
         else:
             click.secho(f"Package: [{name}] not found in group: [{group}]", fg='yellow')
