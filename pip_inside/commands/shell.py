e1c34534c7bcec9cb2230cb7f7e9232c8fe56a6d
orctom <orctom@gmail.com>
2023-02-19 00:21:00 +0800

    update pip for the 1st time

--- pip_inside/commands/shell.py
+++ pip_inside/commands/shell.py
@@ -14,18 +14,19 @@ def handle_shell():
     if os.name != "posix":
         raise Aborted(f"Sorry, only supports *nix, : {os.name}")
 
-    _create_venv()
-    _spaw_new_shell()
+    created = _create_venv()
+    _spaw_new_shell(created)
 
 
 def _create_venv():
     if os.path.exists('.venv'):
-        return
+        return False
     name = Path(os.getcwd()).name
     venv.create('.venv', with_pip=True, prompt=name)
+    return True
 
 
-def _spaw_new_shell():
+def _spaw_new_shell(is_1st_time: bool):
     if os.environ.get('VIRTUAL_ENV') is not None:
         return
     def resize(*args, **kwargs) -> None:
@@ -38,6 +39,8 @@ def _spaw_new_shell():
     if shell.endswith('/zsh'):
         p.setecho(False)
     p.sendline('source .venv/bin/activate')
+    if is_1st_time:
+        p.sendline('pip install -U pip')
     signal.signal(signal.SIGWINCH, resize)
     p.interact(escape_character=None)
     p.close()
