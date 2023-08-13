3691ac6cfad176eef01f9bdd8fae5da26c3d638c
orctom <orctom@gmail.com>
2023-08-13 18:55:03 +0800

    updated "upgrade" using -m

--- pip_inside/commands/upgrade.py
+++ pip_inside/commands/upgrade.py
@@ -8,7 +8,7 @@ from .version import handle_version
 
 def handle_upgrade():
     try:
-        cmd = [(Path(shutil.which('pip-inside')).parent / 'pip').as_posix(), 'install', '-U', 'pip', 'pip-inside']
+        cmd = [(Path(shutil.which('pip-inside')).parent / 'python').as_posix(), '-m', 'pip', 'install', '-U', 'pip', 'pip-inside']
         subprocess.run(cmd, stderr=sys.stderr, stdout=sys.stdout)
         handle_version()
     except subprocess.CalledProcessError:
