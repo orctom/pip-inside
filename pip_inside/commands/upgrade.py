77352278c4738629bfbe210d65ec45995d7d1b5f
orctom <orctom@gmail.com>
2023-08-14 14:00:50 +0800

    rm print version

--- pip_inside/commands/upgrade.py
+++ pip_inside/commands/upgrade.py
@@ -3,13 +3,13 @@ import subprocess
 import sys
 from pathlib import Path
 
-from .version import handle_version
-
 
 def handle_upgrade():
     try:
-        cmd = [(Path(shutil.which('pip-inside')).parent / 'python').as_posix(), '-m', 'pip', 'install', '-U', 'pip', 'pip-inside']
-        subprocess.run(cmd, stderr=sys.stderr, stdout=sys.stdout)
-        handle_version()
+        for cmd in [
+            [shutil.which('python'), '-m', 'pip', 'install', '-U', 'pip'],
+            [(Path(shutil.which('pip-inside')).parent / 'python').as_posix(), '-m', 'pip', 'install', '-U', 'pip', 'pip-inside'],
+        ]:
+            subprocess.run(cmd, stderr=sys.stderr, stdout=sys.stdout)
     except subprocess.CalledProcessError:
         sys.exit(1)
